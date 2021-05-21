97e039e2fee6eaf1215a0d9518373fc60ad3ce46
shimon-home2021 <82209573+shimon-home2021@users.noreply.github.com>
2021-05-21 15:24:29 +0300

    fixed problems now it runs

--- main.py
+++ main.py
@@ -122,8 +122,10 @@ def test():
         error, predicted_label = model.calculate_classification_error(data, bag_label)
         test_error += error
 
-        if batch_idx < 5:  # plot bag labels and instance labels for first 5 bags
-            bag_level = (bag_label.cpu().data.numpy()[0], int(predicted_label.cpu().data.numpy()[0][0]))
+        if batch_idx < 5:
+            pred_lab = predicted_label.cpu().data.numpy()[0]# plot bag labels and instance labels for first 5 bags
+            bag_level = (bag_label.cpu().data.numpy()[0], int(pred_lab))
+            #bag_level = (bag_label.cpu().data.numpy()[0], int(predicted_label.cpu().data.numpy()[0][0]))
             instance_level = list(zip(instance_labels.numpy()[0].tolist(),
                                  np.round(attention_weights.cpu().data.numpy()[0], decimals=3).tolist()))
 
